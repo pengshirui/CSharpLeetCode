1ca0d4d08626c3f948210008ca903b430b0cff8b
shirui.peng <shirui.ryan.peng@gmail.com>
2018-12-10 23:29:34 -0500

    Update

---
+++ LeetCode/Medium/NumberOfIslands.cs
@@ -0,0 +1,55 @@
+﻿/// <summary>
+/// https://leetcode.com/problems/number-of-islands/
+/// </summary>
+namespace LeetCode.Medium
+{
+    public class NumberOfIslands
+    {
+        public int NumIslands(char[,] grid)
+        {
+            if (grid == null || grid.Length == 0)
+            {
+                return 0;
+            }
+
+            int num = 0;
+            for(var i = 0; i < grid.GetLength(0); i++)
+            {
+                for(var j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j] == '1')
+                    {
+                        num++;
+                        Dfs(grid, i, j);
+                    }
+                }
+            }
+            return num;
+        }
+
+        private void Dfs(char[,] grid, int i, int j)
+        {
+            grid[i, j] = '0';
+            // top
+            if (i - 1 >= 0 && grid[i - 1, j] == '1')
+            {
+                Dfs(grid, i - 1, j);
+            }
+            // left
+            if (j - 1 >= 0 && grid[i, j - 1] == '1')
+            {
+                Dfs(grid, i, j - 1);
+            }
+            // right
+            if (j + 1 < grid.GetLength(1) && grid[i, j + 1] == '1')
+            {
+                Dfs(grid, i, j + 1);
+            }
+            // bottom
+            if (i + 1 < grid.GetLength(0) && grid[i + 1, j] == '1')
+            {
+                Dfs(grid, i+ 1, j);
+            }
+        }
+    }
+}
---
+++ UnitTest/Medium/NumberOfIslandsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using LeetCode.Medium;
+using Xunit;
+
+namespace UnitTest.Medium
+{
+    public class NumberOfIslandsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var grid1 = new char[,]{ { '1', '0', '0' }, { '1', '1', '0' }, { '0', '0', '1' } };
+            var grid2 = new char[,] { { '1', '1', '0', '0', '0' }, { '1', '1', '0', '0', '0' },{ '0', '0', '1', '0', '0' },{ '0', '0', '0', '1', '1' } };
+
+            var numberOfIslands = new NumberOfIslands();
+
+            var num1 = numberOfIslands.NumIslands(grid1);
+            var num2 = numberOfIslands.NumIslands(grid2);
+
+            num1.Should().Equals(2);
+            num2.Should().Equals(2);
+        }
+    }
+}
