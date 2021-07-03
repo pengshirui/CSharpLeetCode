986f247001af33a8f6b96c0c243d9da70865e5f4
pengshirui <shirui.ryan.peng@gmail.com>
2021-07-02 23:52:15 -0400

    update

--- LeetCode/ArraysAndStrings/NextPermutation.cs
+++ LeetCode/ArraysAndStrings/NextPermutation.cs
@@ -8,7 +8,42 @@ namespace LeetCode.ArraysAndStrings
     {
         public void NextPermutation(int[] nums)
         {
+            // starts with second last index
+            var i = nums.Length - 2;
+            while (i >= 0 && nums[i + 1] <= nums[i])
+            {
+                i--;
+            }
+            // i stops at index before the peek value
+            if (i >= 0)
+            {
+                var j = nums.Length - 1;
+                while (nums[j] <= nums[i])
+                {
+                    // find index just greater than i
+                    j--;
+                }
+                Swap(nums, i, j);
+            }
+            // reverse from the peek;
+            Reverse(nums, i + 1);
+        }
 
+        private void Reverse(int[] nums, int startIndex) 
+        {
+            var endIndex = nums.Length - 1;
+            while (startIndex < endIndex)
+            {
+                Swap(nums, startIndex++, endIndex--);
+            }
+
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            var t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
         }
     }
 }
---
+++ UnitTest/ArraysAndStrings/NextPermutationTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using LeetCode.ArraysAndStrings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest.ArraysAndStrings
+{
+	public class NextPermutationTest
+	{
+		Solution solution = new Solution();
+
+		[Fact]
+		public void Test1()
+		{
+			var nums1 = new int[] { 1, 2, 3 };
+			var nums2 = new int[] { 3, 2, 1 };
+			var nums3 = new int[] { };
+			var nums4 = new int[] { 1, 1, 5 };
+			var nums5 = new int[] { 1 };
+			solution.NextPermutation(nums1);
+			solution.NextPermutation(nums2);
+			solution.NextPermutation(nums3);
+			solution.NextPermutation(nums4);
+			solution.NextPermutation(nums5);
+
+			nums1.ToString().Should().Be(new int[] { 1, 3, 2 }.ToString());
+			nums2.ToString().Should().Be(new int[] { 1, 2, 3 }.ToString());
+			nums3.ToString().Should().Be(new int[] { }.ToString());
+			nums4.ToString().Should().Be(new int[] { 1, 5, 1 }.ToString());
+			nums5.ToString().Should().Be(new int[] { 1 }.ToString());
+
+		}
+	}
+}
