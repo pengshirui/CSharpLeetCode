ac2e2f19eb67c4bb67b01f14595a82ba05d5cdfd
pengshirui <shirui.ryan.peng@gmail.com>
2021-07-07 00:05:55 -0400

    Add isPalindrome

---
+++ LeetCode/Easy/IsPalindrome.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Easy
+{
+    public partial class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            var l = 0;
+            var r = s.Length - 1;
+
+            while (l < r)
+            {
+                if (!char.IsLetterOrDigit(s[l]))
+                {
+                    l++;
+                }
+                else if (!char.IsLetterOrDigit(s[r]))
+                {
+                    r--;
+                }
+                else
+                {
+                    if (char.ToLower(s[l]) != char.ToLower(s[r]))
+                    {
+                        return false;
+                    }
+                    l++;
+                    r--;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTest/Easy/IsPalindromeTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using LeetCode.Easy;
+using Xunit;
+
+namespace UnitTest.Easy
+{
+    public class IsPalindromeTest
+	{
+
+		Solution solution = new Solution();
+
+		[Fact]
+		public void Test1()
+		{
+			var str1 = "A man, a plan, a canal: Panama";
+			var str2 = "race a car";
+			var str3 = "                  ";
+			var str4 = "aabbaa";
+
+			solution.IsPalindrome(str1).Should().BeTrue();
+			solution.IsPalindrome(str2).Should().BeFalse();
+			solution.IsPalindrome(str3).Should().BeTrue();
+			solution.IsPalindrome(str4).Should().BeTrue();
+
+		}
+	}
+}
