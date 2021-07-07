4992c90fe67e7da01123b7c258703bf46d7b0be5
pengshirui <shirui.ryan.peng@gmail.com>
2021-07-06 23:47:50 -0400

    Add merge

---
+++ UnitTest/Easy/MergeTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using LeetCode.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest.Easy
+{
+    public class MergeTest
+	{
+
+		Solution solution = new Solution();
+
+		[Fact]
+		public void Test1()
+		{
+			var nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+			var nums2 = new int[] { 2, 5, 6 };
+			var nums3 = new int[] { 1 };
+			var nums4 = new int[] { };
+			var nums5 = new int[] { 0 };
+			var nums6 = new int[] { 1 };
+			solution.Merge(nums1, 3, nums2, 3); 
+			nums1.Should().Equal(new int[] { 1, 2, 2, 3, 5, 6 });
+			solution.Merge(nums3, 1, nums4, 0);
+			nums3.Should().Equal(new int[] { 1 });
+			solution.Merge(nums5, 0, nums6, 1);
+			nums5.Should().Equal(new int[] { 1 });
+
+		}
+	}
+}
