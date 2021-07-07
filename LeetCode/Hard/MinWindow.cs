265904bc9a10c28d9161882f586d49646cbf7170
pengshirui <shirui.ryan.peng@gmail.com>
2021-07-06 23:33:36 -0400

    Update

---
+++ LeetCode/Hard/MinWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Hard
+{
+    public partial class Solution
+    {
+        public string MinWindow(string s, string t)
+        {
+            if (s?.Length < t?.Length)
+            {
+                return string.Empty;
+            }
+
+            var minWindowSize = int.MaxValue;
+            var bestL = 0;
+            var l = 0;
+            var r = 0;
+            // (char, (how many total, current number of char in the substring)
+            var kv = new Dictionary<char, int>();
+            foreach(var c in t)
+            {
+                if (kv.TryGetValue(c, out var count))
+                {
+                    kv[c] = count + 1;
+                }
+                else
+                {
+                    kv[c] = 1;
+                }
+            }
+
+            var wkv = new Dictionary<char, int>();
+            var completedCharCount = 0;
+            var totalUniqueCharCount = kv.Count;
+            while (r < s.Length)
+            {
+                
+                var curChar = s[r];
+                if (kv.TryGetValue(curChar, out var requiredCount))
+                {
+                    // hits
+                    var newCount = wkv.TryGetValue(curChar, out var currentCount) ? currentCount : 0;
+                    wkv[curChar] = newCount + 1;
+                    if (requiredCount == wkv[curChar])
+                    {
+                        completedCharCount++;
+                    }
+                }
+
+                while (l <= r && completedCharCount == totalUniqueCharCount)
+                {
+                    if (r - l + 1 < minWindowSize)
+                    {
+                        minWindowSize = r - l + 1;
+                        bestL = l;
+                    }
+                    // keep moving the left pointer
+                    var firstChar = s[l];
+                    if (kv.TryGetValue(firstChar, out var requiredCount2))
+                    {
+                        var newCount2 = wkv[firstChar] - 1;
+                        wkv[firstChar] = newCount2;
+                        if (newCount2 < requiredCount2)
+                        {
+                            completedCharCount--;
+                        }
+                    }
+                    l++;
+                }
+                r++;
+            }
+
+            return minWindowSize == int.MaxValue ? string.Empty : s.Substring(bestL, minWindowSize);
+        }
+    }
+}
---
+++ UnitTest/Hard/MinWindowTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using LeetCode.Hard;
+using Xunit;
+
+namespace UnitTest.Hard
+{
+    public class MinWindowTest
+    {
+
+        Solution solution = new Solution();
+
+        [Fact]
+        public void Test1()
+        {
+            var s1 = "ADOBECODEBANC";
+            var t1 = "ABC";
+            var s2 = "a";
+            var t2 = "a";
+            var s3 = "a";
+            var t3 = "aa";
+            var s4 = "bbaa";
+            var t4 = "aba";
+            solution.MinWindow(s1, t1).Should().Be("BANC");
+            solution.MinWindow(s2, t2).Should().Be("a");
+            solution.MinWindow(s3, t3).Should().Be("");
+            solution.MinWindow(s4, t4).Should().Be("baa");
+        }
+
+    }
+}
