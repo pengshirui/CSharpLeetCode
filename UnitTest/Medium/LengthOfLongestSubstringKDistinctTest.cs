394753d13b3b704955c03602f44ffc77802ff507
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-07-13 21:27:27 -0400

    Add LengthOfLongestSubstringKDistinct

---
+++ UnitTest/Medium/LengthOfLongestSubstringKDistinctTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using LeetCode.Medium;
+using Xunit;
+
+namespace UnitTest.Medium
+{
+	public class LengthOfLongestSubstringKDistinctTest
+	{
+		Solution solution = new Solution();
+
+		[Fact]
+		public void Test1()
+		{
+			var s1 = "abcabcbb";
+			var s2 = "bbbbb";
+			var s3 = "eceba";
+			var s4 = "aa";
+			var s5 = "a";
+			solution.LengthOfLongestSubstringKDistinct(s1, 1).Should().Be(2);
+			solution.LengthOfLongestSubstringKDistinct(s2, 1).Should().Be(5);
+			solution.LengthOfLongestSubstringKDistinct(s3, 2).Should().Be(3);
+			solution.LengthOfLongestSubstringKDistinct(s4, 1).Should().Be(2);
+			solution.LengthOfLongestSubstringKDistinct(s5, 2).Should().Be(1);
+
+		}
+	}
+}
