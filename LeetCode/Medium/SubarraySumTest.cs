26a2e01a8b7cdc5fe2fe0ddaf3ac32869318c5de
Shirui Peng <shirui.ryan.peng@gmail.com>
2021-07-13 23:33:21 -0400

    Add subarry sum

---
+++ LeetCode/Medium/SubarraySumTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    public partial class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            var kv = new Dictionary<int, int>
+            {
+                { 0, 1 },
+            };
+            var sum = 0;
+            var count = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                var diff = sum - k;
+                if (kv.TryGetValue(diff, out var v2))
+                {
+                    count += v2;
+                }
+                if (kv.TryGetValue(sum, out var v1))
+                {
+                    kv[sum] = v1 + 1;
+                }
+                else
+                {
+                    kv.Add(sum, 1);
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/Medium/SubarraySumTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LeetCode.Medium;
+using Xunit;
+
+namespace UnitTest.Medium
+{
+    public class SubarraySumTest
+	{
+		Solution solution = new Solution();
+
+		[Fact]
+		public void Test1()
+		{
+			var nums1 = new int[10];
+			solution.SubarraySum(nums1, 0).Should().Be(55);
+
+			var nums2 = new int[100];
+			solution.SubarraySum(nums2, 0).Should().Be(5050);
+
+		}
+	}
+}
